4869b03c2d90764d8b0560bac65a75c036ac75eb
mathemakitten <helen.ngo14@gmail.com>
2023-01-24 10:38:14 -0500

    run.sh as well as setup

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 cd t5x
 
-python3 -m pip install -e '.[tpu]' -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-
-python3 -m pip install -e '.[pile]'
-
 export PATH=$PATH:/home/helen_huggingface_co/.local/bin
 
 MODEL_DIR=gs://hugginghelen/t5x/experiments/11b/model
