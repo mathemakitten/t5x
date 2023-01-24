a2ea76d848676654ce244b8011e7202a5fde3cbc
mathemakitten <helen.ngo14@gmail.com>
2023-01-24 01:41:48 -0500

    tpu setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+git clone --branch=hn-tpu https://github.com/mathemakitten/t5x
+
+cd t5x
+
+# python3 -m pip install -e '.[tpu]' -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+python3 -m pip install -e '.[pile]'
+
+export PATH=$PATH:/home/helen_huggingface_co/.local/bin
+
+MODEL_DIR=gs://hugginghelen/t5x/experiments/11b/model
+TFDS_DATA_DIR=gs://hugginghelen/t5x-test/pile
+PROJECT_DIR=/home/helen_huggingface_co/t5x/t5x/experiments
+T5X_DIR=/home/helen_huggingface_co/t5x
+
+PYTHONPATH=${PROJECT_DIR} python3 t5x/train.py --gin_search_paths=${PROJECT_DIR} --gin_file="t5x/experiments/t5_1_0_11b.gin" --gin.MODEL_DIR=\"${MODEL_DIR}\" --tfds_data_dir=${TFDS_DATA_DIR}
