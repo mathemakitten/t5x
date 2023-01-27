826a6cb94ed7a1c03e12fe48260902fc18220a3a
mathemakitten <helen.ngo14@gmail.com>
2023-01-27 15:03:47 -0800

    52b config

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ TFDS_DATA_DIR=gs://hugginghelen/t5x-test/pile
 PROJECT_DIR=/home/helen_huggingface_co/t5x/t5x/experiments
 T5X_DIR=/home/helen_huggingface_co/t5x
 
-PYTHONPATH=${PROJECT_DIR} python3 t5x/train.py --gin_search_paths=${PROJECT_DIR} --gin_file="t5x/experiments/t5_1_0_11b.gin" --gin.MODEL_DIR=\"${MODEL_DIR}\" --tfds_data_dir=${TFDS_DATA_DIR}
+#PYTHONPATH=${PROJECT_DIR} python3 t5x/train.py --gin_search_paths=${PROJECT_DIR} --gin_file="t5x/experiments/t5_1_0_11b.gin" --gin.MODEL_DIR=\"${MODEL_DIR}\" --tfds_data_dir=${TFDS_DATA_DIR}
+PYTHONPATH=${PROJECT_DIR} python3 t5x/train.py --gin_search_paths=${PROJECT_DIR} --gin_file="t5x/experiments/decoder_52b.gin" --gin.MODEL_DIR=\"${MODEL_DIR}\" --tfds_data_dir=${TFDS_DATA_DIR}
